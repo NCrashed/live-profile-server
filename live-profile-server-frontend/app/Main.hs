c94085f3eaaf9eda17b5cb674437886cb34247fd
NCrashed <ncrashed@gmail.com>
2016-08-06 17:00:18 +0300

    Draw simple circles in canvas

--- live-profile-server-frontend/app/Main.hs
+++ live-profile-server-frontend/app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Graphics.Shine
+import Graphics.Shine.Picture
+import Graphics.Shine.Render
+import GHCJS.DOM
+
+concentricCircles :: Picture
+concentricCircles = Translate 100 100 $ foldMap CircleF [1, 10..100]
+
 main :: IO ()
-main = return ()
+main = runWebGUI $ \ webView -> do
+    ctx <- fixedSizeCanvas webView 1920 900
+    render ctx concentricCircles
