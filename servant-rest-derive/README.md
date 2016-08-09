0c04a14a39865b8eb4cd37a88c9d325f0e37fde1
NCrashed <ncrashed@gmail.com>
2016-08-09 13:55:31 +0300

    Extract REST API deriving into servant-rest-derive package

---
+++ servant-rest-derive/README.md
@@ -0,0 +1,2 @@
+# servant-rest-derive
+
---
+++ servant-rest-derive/src/Servant/API/REST/Derive.hs
@@ -0,0 +1,52 @@
+module Servant.API.REST.Derive(
+    Id(..)
+  , RESTAction(..)
+  , RESTPermission
+  , PatchRec
+  , RESTFull
+  ) where 
+
+import Data.Aeson.Unit
+import Data.Aeson.WithField
+import GHC.Generics
+import GHC.TypeLits 
+import Servant.API 
+import Servant.API.Auth.Token 
+
+-- | Unique id of resource
+newtype Id a = Id { unId :: Word } 
+  deriving (Show, Eq, Generic)
+
+-- | Type of action that is permited by a REST API
+data RESTAction = Read | Write | Create | Delete 
+  deriving (Show, Eq, Generic)
+
+-- | Calculate permission labels for rest action
+type family RESTPermission (t :: RESTAction) (a :: Symbol) :: PermSymbol where 
+  RESTPermission 'Read a = 'PermConcat (PermLabel "read-") (PermLabel a)
+  RESTPermission 'Write a = 'PermConcat (PermLabel "write-") (PermLabel a)
+  RESTPermission 'Create a = 'PermConcat (PermLabel "create-") (PermLabel a)
+  RESTPermission 'Delete a = 'PermConcat (PermLabel "delete-") (PermLabel a)
+
+-- | Corresponding data type with patch data for 'a'
+type family PatchRec a
+
+-- | Generation of REST-full API
+type RESTFull a (aname :: Symbol) = 
+       Capture "id" (Id a)
+    :> TokenHeader '[RESTPermission 'Read aname]
+    :> Get '[JSON] a
+  :<|> ReqBody '[JSON] a 
+    :> TokenHeader '[RESTPermission 'Create aname]
+    :> Post '[JSON] (OnlyId a)
+  :<|> Capture "id" (Id a)
+    :> ReqBody '[JSON] a 
+    :> TokenHeader '[RESTPermission 'Write aname]
+    :> Put '[JSON] Unit 
+  :<|> Capture "id" (Id a)
+    :> ReqBody '[JSON] (PatchRec a)
+    :> TokenHeader '[RESTPermission 'Write aname]
+    :> Patch '[JSON] Unit
+  :<|> Capture "id" (Id a)
+    :> TokenHeader '[RESTPermission 'Delete aname]
+    :> Delete '[JSON] Unit
