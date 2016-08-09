cffb32f11ed38d2497882bb37de4febd018ac2d9
NCrashed <ncrashed@gmail.com>
2016-08-09 20:51:05 +0300

    Implement next layer of server deriving

--- servant-rest-derive-server/src/Servant/API/REST/Derive/Server.hs
+++ servant-rest-derive-server/src/Servant/API/REST/Derive/Server.hs
@@ -9,24 +9,47 @@ Portability : Portable
 -}
 module Servant.API.REST.Derive.Server(
     RESTServer
+  , HasRESTDB(..)
+  , StorableResource(..)
   , HasRESTServer(..)
   ) where 
 
+import Control.Monad.Except 
 import Data.Aeson.Unit
 import Data.Aeson.WithField
+import Data.Monoid 
+import Data.Proxy 
+import Database.Persist.Sql 
+import GHC.TypeLits 
 import Servant.API 
 import Servant.API.Auth.Token 
 import Servant.API.REST.Derive
 import Servant.Server 
---import Servant.Server.Auth.Token 
+import Servant.Server.Auth.Token
 
+import qualified Data.ByteString.Lazy.Char8 as B
+
+-- | Shortcut for server that handles REST API that was geerated by 'RESTFull' type
 type RESTServer a aname m = ServerT (RESTFull a aname) m
+
+-- | Defines server handlers that has access to RDBMS
+class HasRESTDB m where 
+  runRESTDB :: SqlPersistT m a -> m a
+
+-- | Operations for 'a' to read/insert/update/delete in RDBMS
+class StorableResource m a where 
+  readResource :: Id a -> SqlPersistT m (Maybe a)
+  insertResource :: a -> SqlPersistT m (Id a)
+  replaceResource :: Id a -> a -> SqlPersistT m ()
+  patchResource :: Id a -> PatchRec a -> SqlPersistT m ()
+  deleteResource :: Id a -> SqlPersistT m ()
 
 -- | Typeclass that derives server implementation
 class HasRESTServer a aname m where 
   restServer :: ServerT (RESTFull a aname) m
 
-instance HasRESTServer a aname m where 
+instance (KnownSymbol aname, HasRESTDB m, StorableResource m a, MonadError ServantErr m, AuthMonad m) 
+  => HasRESTServer a aname m where 
   restServer = 
          getRes
     :<|> postRes
@@ -34,29 +57,45 @@ instance HasRESTServer a aname m where
     :<|> patchRes 
     :<|> deleteRes
     where 
+    aname = B.pack $ symbolVal (Proxy :: Proxy aname)
+
     getRes :: Id a 
       -> MToken '[RESTPermission 'Read aname]
       -> m a 
-    getRes = undefined 
+    getRes i token = do 
+      guardAuthToken token 
+      ma <- runRESTDB $ readResource i
+      maybe (throwError $ err404 { errBody = "Cannot find resource " <> aname })
+        return ma 
 
     postRes :: a 
       -> MToken '[RESTPermission 'Create aname]
       -> m (OnlyId (Id a))
-    postRes = undefined
+    postRes a token = do 
+      guardAuthToken token 
+      i <- runRESTDB $ insertResource a 
+      return $ OnlyField i 
 
-    putRes :: Id a 
-      -> a 
+    putRes :: Id a -> a 
       -> MToken '[RESTPermission 'Write aname]
       -> m Unit 
-    putRes = undefined
+    putRes i a token = do
+      guardAuthToken token 
+      runRESTDB $ replaceResource i a 
+      return Unit
 
-    patchRes :: Id a
-      -> PatchRec a 
+    patchRes :: Id a -> PatchRec a 
       -> MToken '[RESTPermission 'Write aname]
       -> m Unit 
-    patchRes = undefined
+    patchRes i pa token = do 
+      guardAuthToken token 
+      runRESTDB $ patchResource i pa 
+      return Unit
 
     deleteRes :: Id a 
       -> MToken '[RESTPermission 'Delete aname]
       -> m Unit
-    deleteRes = undefined
+    deleteRes i token = do 
+      guardAuthToken token 
+      runRESTDB $ deleteResource i 
+      return Unit
