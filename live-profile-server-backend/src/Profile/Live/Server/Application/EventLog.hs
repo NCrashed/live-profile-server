75c40b6f0509371085f56a9a2243efa47737c800
NCrashed <ncrashed@gmail.com>
2016-08-25 01:28:49 +0300

    Add func to store state

--- live-profile-server-backend/src/Profile/Live/Server/Application/EventLog.hs
+++ live-profile-server-backend/src/Profile/Live/Server/Application/EventLog.hs
@@ -13,9 +13,11 @@ module Profile.Live.Server.Application.EventLog(
   , addEventLogEvent
   ) where 
 
+import Control.Monad 
 import Database.Persist.Sql 
 import GHC.RTS.Events
 
+import Profile.Live.Protocol.State
 import Profile.Live.Server.Events.Model
 
 -- | Insert empty eventlog
@@ -29,3 +31,18 @@ addEventLogType lid t = insert $ toEventTypeImpl lid t
 -- | Insert event into eventlog being recording
 addEventLogEvent :: EventLogImplId -> Event -> SqlPersistT IO EventImplId 
 addEventLogEvent lid e = insert $ toEventImpl lid e
+
+-- | Insert state snapshot into eventlog being recording
+addEventLogState :: EventLogImplId -> EventlogState -> SqlPersistT IO EventlogStateImplId
+addEventLogState lid s = do 
+  let (impl, threads, capsets, caps, tasks) = toEventlogStateImpl lid s 
+  si <- insert impl 
+  mapM_ (void . insert) $ threads si
+  forM_ (capsets si) $ \(cs, caps, args, envs) -> do 
+    ci <- insert cs 
+    mapM_ (void . insert) $ caps ci
+    mapM_ (void . insert) $ args ci 
+    mapM_ (void . insert) $ envs ci 
+  mapM_ (void . insert) $ caps si 
+  mapM_ (void . insert) $ tasks si
+  return si
