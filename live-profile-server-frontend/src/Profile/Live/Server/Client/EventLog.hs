42594089ee80e3b14f5fc66e0c2e1c5db098880e
NCrashed <ncrashed@gmail.com>
2016-08-28 14:48:13 +0300

    Frontend implementation of eventlog api

--- live-profile-server-frontend/src/Profile/Live/Server/Client/EventLog.hs
+++ live-profile-server-frontend/src/Profile/Live/Server/Client/EventLog.hs
@@ -2,15 +2,32 @@ module Profile.Live.Server.Client.EventLog(
     eventLogWidget
   ) where 
 
+import Control.Monad.Trans.Either
+import GHCJS.Marshal
 import Reflex 
 import Reflex.Dom 
-
 import Servant.API.Auth.Token 
+import Servant.API.Auth.Token.Pagination
 import Servant.API.REST.Derive 
+import Servant.Client 
 
+import qualified GHC.RTS.Events as E 
+
+import Profile.Live.Server.API.EventLog
 import Profile.Live.Server.API.Session
-import Profile.Live.Server.Client.Router 
+import Profile.Live.Server.Client.Async
 import Profile.Live.Server.Client.Bootstrap.Button 
+import Profile.Live.Server.Client.Router 
+
+-- | Getting list of events
+eventsList :: EventLogId 
+  -> Maybe Page 
+  -> Maybe PageSize
+  -> MToken' '["read-eventlog"]
+  -> EitherT ServantError IO (PagedList (Id E.Event) E.Event) 
+
+( eventsList 
+  ) = client eventLogAPI Nothing
 
 -- | Widget to display raw eventlog
 eventLogWidget :: MonadWidget t m 
